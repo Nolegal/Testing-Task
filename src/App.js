6c420808b410555d065124ee2fe83d260bd7b80e
Nolegal <nolegal@ukr.net>
2021-09-07 12:26:10 +0300

    Testing Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from './assets/data'
 import './App.css';
 
 function App() {
+  const [array, SetArray] = useState(data)
+  const [list, SetList] = useState('')
+  const NameSort = () => {
+    SetArray(array.sort((a, b) => parseInt(a.name) - parseInt(b.name)));
+
+  };
+  const Pages = () => {
+    SetArray(array.sort((a, b) => parseInt(a.pageviews) - parseInt(b.pageviews)));
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="row">
+
+        <input
+          type="text"
+          placeholder="Поиск авторов по имени"
+          value={list}
+          onChange={(e) => {
+            const Searchlist = array.filter(data => {
+              return data.name.includes(e.target.value)
+            });
+
+            SetArray(Searchlist);
+            SetList(e.target.value);
+          }}
+        />
+        <button onClick={NameSort}>Имя</button>
+        <button onClick={Pages}>Просмотры </button>
+        {array.map((data) => (
+          <div className="block" key={data.name}>
+            <div> {data.name}
+
+
+              <p>{data.count_pub}публ.</p>
+
+
+            </div>
+            <img alt="" />
+            <div>
+              {data.pageviews}
+            </div>
+
+
+          </div>
+
+        )
+
+        )}
+
+      </div>
+
+
+    </>
   );
 }
 
